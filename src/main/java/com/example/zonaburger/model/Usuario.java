fb2132e69ccc43ae6b02712aaed9bee8123955bf
Tavo488 <tavo488.leo@gmail.com>
2026-09-20 23:00:14 -0500

    Le cambie a minusculas el model y service

---
+++ src/main/java/com/example/zonaburger/model/Usuario.java
@@ -0,0 +1,26 @@
+package com.example.zonaburger.model;
+
+public class Usuario {
+
+    private String usuario;
+    private String contrasena;
+    private String rol;
+
+    public Usuario(String usuario, String contrasena, String rol) {
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+}
